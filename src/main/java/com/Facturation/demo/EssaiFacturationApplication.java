37fb8dd3eb24440d8a708ce1890b091867546608
Chebel405 <anne.chebel@gmail.com>
2023-03-30 16:46:35 +0200

    fait

---
+++ src/main/java/com/Facturation/demo/EssaiFacturationApplication.java
@@ -0,0 +1,13 @@
+package com.Facturation.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssaiFacturationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssaiFacturationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Facturation/demo/EssaiFacturationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Facturation.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssaiFacturationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
